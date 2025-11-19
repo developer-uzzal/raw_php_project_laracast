bd5094c70c9098c62bf2429bb3712f6e01e1bbb8
dev-uzzal <mduzzal999111@gmail.com>
2025-11-19 17:03:47 +0600

    update

---
+++ Database.php
@@ -0,0 +1,28 @@
+<?php
+// connect to our mysql database
+
+class Database
+{
+    public $connection;
+
+    public function __construct($config, $username ='root', $password='1234'){
+        
+        $dsn ='mysql:' . http_build_query($config,'',';');
+
+        // Data Source Name
+        // $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset={$config['charset']}";
+
+        $this->connection = new PDO($dsn, $username, $password,[
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query($query)
+    {
+        $statement = $this->connection->prepare($query);
+
+        $statement->execute();
+
+        return $statement;
+    }
+}
--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
 
 require "function.php";
-require "router.php";
+// require "router.php";
+require "Database.php";
+
+$config = require "config.php";
+
+$db = new Database($config['database']);
+$posts = $db->query("select * from posts")->fetchAll();
+
+
+
+dd($posts);
